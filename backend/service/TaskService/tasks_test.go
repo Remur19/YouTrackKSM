e0e74ad071685e1439b6fd8795b9a73482a40c63
Witzler <chrisavril24@gmail.com>
2025-05-12 17:52:33 +0200

    Tests for 2 Task functions

---
+++ backend/service/TaskService/tasks_test.go
@@ -0,0 +1,41 @@
+package taskservice
+
+import (
+	"backend/repo"
+	"backend/utils"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+var (
+	mockTaskRepo = repo.NewMockRepo()
+	taskService  = NewTaskService(mockTaskRepo)
+)
+
+func TestCreateTask(t *testing.T) {
+	err := taskService.CreateTask(utils.Task{
+		Title: "Testaufgabe",
+	})
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestGetTasksByUser(t *testing.T) {
+	userID := uuid.New()
+
+	taskService.CreateTask(utils.Task{
+		Title:    "UserTask1",
+		Owner_id: userID,
+	})
+	taskService.CreateTask(utils.Task{
+		Title:    "UserTask2",
+		Owner_id: userID,
+	})
+
+	tasks, err := taskService.GetTasksByUser(userID)
+	if err != nil || len(tasks) != 2 {
+		t.Fail()
+	}
+}
